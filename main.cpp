d34e766b68d67ccfe948773bc42f02773f842ebe
Scrize <sacrize@gmail.com>
2018-01-15 14:46:05 +0100

    main loop

--- main.cpp
+++ main.cpp
@@ -2,35 +2,38 @@
 
 int main()
 {
-	int n = 0;
-
-	std::cout << "Number of numbers: ";
-	std::cin >> n;
-	std::cout << std::endl;
-
-	int *tab = new int[n];
-
-	std::cout << "Give me all the numbers:" << std::endl;
-
-	for (int i = 0; i < n; i++)
+	while (true)
 	{
-		std::cout << "No. " << i + 1 << ": ";
-		std::cin >> tab[i];
+		int n = 0;
+
+		std::cout << "Number of numbers: ";
+		std::cin >> n;
+		std::cout << std::endl;
+
+		int *tab = new int[n];
+
+		std::cout << "Give me all the numbers:" << std::endl;
+
+		for (int i = 0; i < n; i++)
+		{
+			std::cout << "No. " << i + 1 << ": ";
+			std::cin >> tab[i];
+		}
+
+		Quick quick(tab, n);
+
+		std::cout << std::endl;
+
+		for (int i = 0; i < n; i++) 
+		{
+			std::cout << tab[i];
+			if (i < n - 1) std::cout << ", ";
+		}
+
+		std::cout << std::endl;
+
+		delete[] tab;
 	}
-
-	Quick quick(tab, n);
-
-	std::cout << std::endl;
-
-	for (int i = 0; i < n; i++) 
-	{
-		std::cout << tab[i];
-		if (i < n - 1) std::cout << ", ";
-	}
-
-	std::cout << std::endl;
-
-	delete[] tab;
 
 	system("PAUSE");
 	return 0;
