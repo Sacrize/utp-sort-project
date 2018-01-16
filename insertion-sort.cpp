2f9aad5f85aadf4f191d52c0924b03722e08dbe0
Patryk <patryk.syka@gmail.com>
2018-01-16 17:21:53 +0100

    Dodałem algorytm sortujący

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int insertionSort(int tab[], int n) {
-		if (n < 2) {
-			cout << "Nothing to sort...";
+	if (n < 2) {
+		cout << "Nothing to sort...";
+		return 1;
+	}
+	for (int i = 1; i < n; i++) {
+		int temp = tab[i];
+		for (int k = i; k > 0 && tab[k - 1] > temp; k--) {
+			tab[k] = tab[k - 1];
+			tab[k - 1] = temp;
+		}
+	}
 	return 0;
-}
