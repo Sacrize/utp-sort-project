7bafa2bb610264ae0a276ef67ce7eebfc2530dd9
aw0lf <adrianna.tycner@yahoo.com>
2018-01-19 12:26:57 +0100

    Kosmetyka czytelnosci kodu

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -6,15 +6,15 @@ int selectionSortMax(int t[], int t_s);
 int selectionSort(int t[], int t_s);
 int test();
 
-/*Wybor przez uzytkownika
-kolejnosci sortowania*/
-int test() {
+
+int test() { //test
 	int t_size;
 	cout << "Podaj ilosc liczb do sortowania" << endl;
 	cin >> t_size;
 		int*tab = new int[t_size];
 		cout << "wymien liczby do posortowania" << endl;
-		for (int i = 0; i < t_size; i++) {
+		for (int i = 0; i < t_size; i++)
+		{
 			cin >> tab[i];
 		}
 		selectionSort(tab, t_size);
@@ -23,6 +23,8 @@ int test() {
 		return tab[t_size];
 }
 
+/*Wybor przez uzytkownika
+kolejnosci sortowania*/
 int selectionSort(int t[], int t_s)
 {
 	int a; //zmienna wyboru
@@ -31,7 +33,8 @@ int selectionSort(int t[], int t_s)
 	cin >> a;
 	if (a == 1 || 2)
 	{
-		if (a == 1) {//uzyt wybral selectionsortMin
+		if (a == 1)
+		{//uzyt wybral selectionsortMin
 			selectionSortMin(t, t_s);
 		}
 		else if (a == 2)//uzyt wybral selectionSortMax
@@ -53,24 +56,26 @@ int selectionSortMin(int t[], int t_s)
 	elementu
 	*/
 	int min; //najmniejszy element
-	for (int i = 0; i < (t_s - 1); i++) { //pêtla leci po wszystkich elementach tablicy, -1 bo bêd¹ porównywane 2 elem.
-		min = i;//ustawiam min. elem. na aktualn¹ pozycje
-		for (int j = i + 1; j < t_s; j++) //i+1-aktualny elem. porównywany z nastêpnym indexem
+	for (int i = 0; i < (t_s - 1); i++) //pêtla leci po wszystkich elementach tablicy, -1 bo bêd¹ porównywane 2 elem.
 		{
-			if (t[j] < t[min]) {
-				min = j; //jesli nastepny elem bêdzie < od aktualnieustawionego min. elem.-zamieniamy je
+			min = i;//ustawiam min. elem. na aktualn¹ pozycje
+			for (int j = i + 1; j < t_s; j++) //i+1-aktualny elem. porównywany z nastêpnym indexem
+			{
+				if (t[j] < t[min])
+				{
+					min = j; //jesli nastepny elem bêdzie < od aktualnieustawionego min. elem.-zamieniamy je
+				}
 			}
-		}
 		/*Wstawianie
 		min elem na miejse*/
 
-		if (min != 1) //jesli akt. min. elem. nie jest równy akt. iteracji glownego for
-		{
-			int temp = t[i]; //zmienna pomoc, ktorej przypisuje indeks akt iteracji
-			t[i] = t[min]; //akt. iteracji przypisuje wartosc najmniejszego elem.
-			t[min] = temp;//najmnniekszy elem. dostaje indeks akt. iteracji
+			if (min != 1) //jesli akt. min. elem. nie jest równy akt. iteracji glownego for
+			{
+				int temp = t[i]; //zmienna pomoc, ktorej przypisuje indeks akt iteracji
+				t[i] = t[min]; //akt. iteracji przypisuje wartosc najmniejszego elem.
+				t[min] = temp;//najmnniekszy elem. dostaje indeks akt. iteracji
+			}
 		}
-	}
 	return t[t_s];
 }
 /*Sortowanie 
@@ -85,7 +90,9 @@ int selectionSortMax(int t[], int t_s) //analogiczne nie selectionSortMin, zamia
 		for (int j = i + 1; j < t_s; j++)
 		{
 			if (t[j] > t[max])//jesli elem. bedzie wiekszy od akt. max elem.- zamieniamy je
+			{
 				max = j;
+			}
 		}
 		if (max != i)
 		{
