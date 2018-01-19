ca0c40d5522ce433fdee683574100a8caa24f2ec
aw0lf <adrianna.tycner@yahoo.com>
2018-01-19 12:17:28 +0100

    test

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-void selectionSortMin(int t[], int t_s);
-void selectionSortMax(int t[], int t_s);
-void selectionSort(int t[], int t_s);
+int selectionSortMin(int t[], int t_s);
+int selectionSortMax(int t[], int t_s);
+int selectionSort(int t[], int t_s);
+int test();
 
 /*Wybor przez uzytkownika
 kolejnosci sortowania*/
+int test() {
+	int t_size;
+	cout << "Podaj ilosc liczb do sortowania" << endl;
+	cin >> t_size;
+		int*tab = new int[t_size];
+		cout << "wymien liczby do posortowania" << endl;
+		for (int i = 0; i < t_size; i++) {
+			cin >> tab[i];
+		}
+		selectionSort(tab, t_size);
+		
+		delete tab;
+		return tab[t_size];
+}
 
-void selectionSort(int t[], int t_s)
+int selectionSort(int t[], int t_s)
 {
 	int a; //zmienna wyboru
 	
 	cout << "Wybierz czy chesz posortowac liczby malejaco czy rosnaco.\n\t-jesli od najmniejszej do najwiekszej, wybierz 1\n\t-jesli od najwiekszej do najmniejszej, wybierz 2\n";
 	cin >> a;
-	if (a = 1 || 2)
+	if (a == 1 || 2)
 	{
-		if (a = 1) {//uzyt wybral selectionsortMin
+		if (a == 1) {//uzyt wybral selectionsortMin
 			selectionSortMin(t, t_s);
 		}
-		else if (a = 2)//uzyt wybral selectionSortMax
+		else if (a == 2)//uzyt wybral selectionSortMax
 		{
 			selectionSortMax(t, t_s);
 		}
 	}
-		else//zostaly wprowadzone bledne znaki
-		{
-			cout << "Wprowadziles bledny znak" << endl;
-		}
-
 	
-	return;
+	return t[t_s];
 }
 
 /*Sortowanie
 najmniejszy-najwiekszy elm.*/
 
-void selectionSortMin(int t[], int t_s)
+int selectionSortMin(int t[], int t_s)
 {
 	/*Szukanie
 	najmniejszego
@@ -61,12 +71,12 @@ void selectionSortMin(int t[], int t_s)
 			t[min] = temp;//najmnniekszy elem. dostaje indeks akt. iteracji
 		}
 	}
-	return;
+	return t[t_s];
 }
 /*Sortowanie 
 najwiekszy-najmniejszy elem.*/
 
-void selectionSortMax(int t[], int t_s) //analogiczne nie selectionSortMin, zamiast zmiennej min jest max i zmiana linijki 21
+int selectionSortMax(int t[], int t_s) //analogiczne nie selectionSortMin, zamiast zmiennej min jest max i zmiana linijki 21
 {
 	int max;
 	for (int i = 0; i < (t_s - 1); i++)
@@ -84,4 +94,5 @@ void selectionSortMax(int t[], int t_s) //analogiczne nie selectionSortMin, zami
 			t[max] = temp;
 		}
 	}
+	return t[t_s];
 }
