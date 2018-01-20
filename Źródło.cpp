b2f61a6d2511777b40909383ab32dca6c55d024e
kitsune242 <lukasz252a@gmail.com>
2018-01-20 12:45:44 +0100

    dodadanie sortowanie malejacego i rosnacego

---
+++ Źródło.cpp
@@ -0,0 +1,21 @@
+﻿#include "iostream"
+
+using namespace std;
+
+void sortowanie_mal(int tab[], int n)
+{
+	for (int i = 0; i<n; i++)
+		for (int j = 1; j<n - i; j++) //pętla wewnętrzna
+			if (tab[j - 1]>tab[j])
+
+				swap(tab[j], tab[j - 1]);//zamiana miejscami
+
+}
+
+void sortowanie_ros(int tab[], int n)
+{
+	for (int i = 0; i<n; i++)
+		for (int j = 1; j<n - i; j++) //pętla wewnętrzna
+			if (tab[j - 1]<tab[j])
+
+				swap(tab[j], tab[j - 1]);//zamiana miejscami
